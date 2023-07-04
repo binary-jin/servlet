2903b8e3049d84ccb3820fff60d1e1d6afd888f4
yi-jin <dlwls4338@naver.com>
2023-07-04 15:51:03 +0900

    frontController를 통해 접근 가능하게 한 것에서 MyView를 통해 viewPath 겹치는 부분을 줄이고 코드를 정리함

---
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -0,0 +1,24 @@
+package hello.servlet.web.frontcontroller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MyView {
+
+    //기존 컨트롤러에서 viewPath로 일일이 설정하던 부분은 MyView로 통합
+
+    private String viewPath;
+
+    public MyView(String viewPath) {
+        this.viewPath = viewPath;
+    }
+
+    public void render(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+    }
+}
