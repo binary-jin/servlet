e23f81566423dc02edadf20bccd30e0b1805a918
yi-jin <dlwls4338@naver.com>
2023-07-05 02:17:35 +0900

    ModelView를 만들어 model에 담아 값을 넘김, 논리적인 view의 값만 컨트롤러에서 지정하고 나머지는 viewResolver에게 넘김

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+    private String viewName; //modelview에서 view 부분
+    private Map<String, Object> model = new HashMap<>(); //modelview에서 mdoel부분
+
+    public ModelView(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+}
